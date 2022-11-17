7fbc1a2bb75f44ff028c4957b5554a0089edf0f5
Daniel Seniv <danielseniv@outlook.com>
2022-11-17 03:38:02 +0000

    Simple Assert statment comparing string/numbers, and returning if the case fail or pass

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  let pass = "✅✅✅ Assertion Passed: [" + actual + "] === [" + expected + "]";
+  let fail = "🛑🛑🛑 Assertion Failed: [" + actual + "] !== [" + expected + "]";
+  return (actual === expected ? console.log(pass) : console.log(fail));
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("See","See");
+assertEqual(2,3);
