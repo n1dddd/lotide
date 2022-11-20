1b720328e3c83aad9ff16bf1f29ecc5705af3de7
Daniel Seniv <danielseniv@outlook.com>
2022-11-20 19:32:41 +0000

    Compare values/types of elements in array perfectly, nested arrays or objects not included. Assertion statement testing.

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  let pass = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  let fail = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  return (actual === expected ? console.log(pass) : console.log(fail));
+};
+
+const eqArrays = function(array1,array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false)// => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
