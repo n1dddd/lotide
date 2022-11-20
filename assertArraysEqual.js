af9aa38d4fd11bc917d3a6c7f88c42de4c0b9f57
Daniel Seniv <danielseniv@outlook.com>
2022-11-20 19:55:57 +0000

    Logic fix

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,17 +3,17 @@ const eqArrays = function(array1,array2) {
     console.log("The two arrays are not equal!")
     return false;
   }
-
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       console.log("The two arrays are not equal!")
       return false;
     }
+    if (array1[i] === array2[i]) {
+      console.log("The two arrays are equal!")
+      return true;
+    }
   }
-  console.log("The two arrays are equal!")
-  return true;
 }
-
 eqArrays([1,2,3],[1,2,3]);
 eqArrays([1,2,3],[1,2,3,4]);
 eqArrays([1,2,4],[1,2,3]);
