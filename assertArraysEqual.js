696aa166e28ec14cd6078f65da42b2469e2fab17
Daniel Seniv <danielseniv@outlook.com>
2022-11-20 20:22:12 +0000

    Functionality restructure. Implement the eqArray function.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,23 @@
 const eqArrays = function(array1,array2) {
   if (array1.length !== array2.length) {
-    console.log("The two arrays are not equal!");
     return false;
   }
+
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      console.log("The two arrays are not equal!");
       return false;
     }
-    if (array1[i] === array2[i]) {
-      console.log("The two arrays are equal!");
-      return true;
-    }
+  }
+  return true;
+};
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1,array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
-eqArrays([1,2,3],[1,2,3]);
-eqArrays([1,2,3],[1,2,3,4]);
-eqArrays([1,2,4],[1,2,3]);
-eqArrays([1,2,4],[1,2,'4']);
+assertArraysEqual([1,2,3],[1,2,3]);
+assertArraysEqual([1,2,3],[1,2,3,4]);
+assertArraysEqual([1,2,4],[1,2,3]);
+assertArraysEqual([1,2,4],[1,2,'4']);
