48f4d2bde18fbc7bd75259bedb494094640854b1
Daniel Seniv <danielseniv@outlook.com>
2022-11-22 03:12:32 +0000

    Define function. Add assertion test case

---
+++ letterPositions.js
@@ -0,0 +1,28 @@
+const eqArrays = function(array1,array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1,array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  return results;
+}
+const result1 = letterPositions("lighthouse in the house");
+
+assertArraysEqual(result1[h] , [3,5,15,18])
