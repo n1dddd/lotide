083003c74241a90f1a10b0140beceb3440601e18
Daniel Seniv <danielseniv@outlook.com>
2022-11-22 03:01:35 +0000

    Function to keep count of character in string, store resultant totals in object

--- countLetters.js
+++ countLetters.js
@@ -8,14 +8,27 @@ const countLetters = function(sentenceToCount) {
   const results = {};
 
   for (const letter of sentenceToCount) {
-    if (results[letter]) {
-      results[letter] += 1;
-    } else {
-      results[letter] = 1;
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
     }
   }
-  return console.log(results);
+  return results;
 };
 
-countLetters("lighthouse in the house");
+const results1 = countLetters("lighthouse in the house");
+assertEqual(results1["l"],1);
+assertEqual(results1["i"],2);
+assertEqual(results1["g"],1);
+assertEqual(results1["h"],4);
+assertEqual(results1["t"],2);
+assertEqual(results1["o"],2);
+assertEqual(results1["u"],2);
+assertEqual(results1["s"],2);
+assertEqual(results1["e"],3);
+assertEqual(results1["n"],1);
+
 
