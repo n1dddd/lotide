56d382fe83f11db7fbe3672afff870aca9ead29f
Daniel Seniv <danielseniv@outlook.com>
2022-11-23 22:24:16 +0000

    Return index position for characters in a string, including repeating characters. Pass values into object as key pairs.

--- letterPositions.js
+++ letterPositions.js
@@ -18,11 +18,20 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-const letterPositions = function(sentence) {
-  const results = {};
-
+const letterPositions = function(sentence) { //takes in parameter string
+  const results = {}; //empty object to store key pairs
+  for (let i = 0; i < sentence.length; i++) { //loops through string
+    if (sentence[i] !== ' ') { //ignores spaces
+      if (results[sentence[i]]) { //if the index value is the seen again throughout the loop
+        results[sentence[i]].push(i); //pushes the index(s) into the object key, and stored as an array
+      } else {
+        results[sentence[i]] = [i]; //if only 1 was seen index, provides index value to array
+      }
+    }
+  }
   return results;
-}
-const result1 = letterPositions("lighthouse in the house");
+};
+assertArraysEqual(letterPositions("lighthouse in the house").h, [3,5,15,18]);
 
-assertArraysEqual(result1[h] , [3,5,15,18])
+
+
