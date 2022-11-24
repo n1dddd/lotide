778fab9b500e5e70caed4642dedd7063f9a39f97
Daniel Seniv <danielseniv@outlook.com>
2022-11-24 03:30:33 +0000

    Compare 2 objects, and if 1 does not match the other, return false, otherwise, return true

--- eqObjects.js
+++ eqObjects.js
@@ -7,18 +7,22 @@ const assertEqual = function(actual, expected) {
 const eqObjects = function(object1,object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
-  } else {
-    return true;
   }
+  for (const properties of Object.keys(object1)) {
+    if (object1[properties] !== object2[properties]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const ab = {a:"1",b:"2"};
 const ba = {a:"1",b:"2"};
 const abc = {a:"1",b:"2",c:"3"};
 
-console.log(eqObjects(abc,ba));
+console.log(eqObjects(ab,ba));
 
 
 // eqObjects(ab,abc);
-// assertEqual(eqObjects(ab,ba),true);
+//assertEqual(eqObjects(abc,ba),false);
 // assertEqual(eqObjects(ab,abc),false);
