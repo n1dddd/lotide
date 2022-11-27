f2e293ed1cf7109fa2d99707d4fd9826be6c3f4a
Daniel Seniv <danielseniv@outlook.com>
2022-11-27 15:48:21 -0500

    Take in keys of object, loop through, pass value to callback, based on boolean returned from callback, return value in key-value pairs up until they are truthy

---
+++ findKey.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  let pass = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  let fail = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  return (actual === expected ? console.log(pass) : console.log(fail));
+};
+const findKey = function(object,callback) {
+  for (const stars in object) {
+    if (callback(object[stars])) {
+      return stars;
+    }
+  }
+  return undefined;
+};
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+assertEqual(result1,"noma");
+
+const result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3); // => "Akaleri"
+assertEqual(result2,"Akaleri");
+
