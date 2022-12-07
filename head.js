909847b0807e13c2771b90cee3816b6f84b441e5
Daniel Seniv <danielseniv@outlook.com>
2022-12-07 04:26:53 +0000

    Refactor code -- include require -- create test file(s) to seperate testing from function

--- head.js
+++ head.js
@@ -1,8 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let pass = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  let fail = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  return (actual === expected ? console.log(pass) : console.log(fail));
-};
 function head(array) {
   if (array === undefined) {
     return undefined;
@@ -11,6 +6,4 @@ function head(array) {
   return firstElement;
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]),5);
+module.exports = head
