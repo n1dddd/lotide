e8a0594fe5dc0d4115f2121a5fef41a33bf06449
Daniel Seniv <danielseniv@outlook.com>
2022-12-07 04:50:54 +0000

    Refactor code -- include require to export function as module -- create test file(s) to seperate testing from function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,5 @@
-const eqArrays = function(array1,array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
+const eqArrays = require('./eqArrays');
 
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1,array2)) {
     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
@@ -17,7 +7,5 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
-assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual([1,2,3],[1,2,3,4]);
-assertArraysEqual([1,2,4],[1,2,3]);
-assertArraysEqual([1,2,4],[1,2,'4']);
+
+module.exports = assertArraysEqual;
