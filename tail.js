7f546fc579ef55cd2685ac5684e1ca8eace42ece
Daniel Seniv <danielseniv@outlook.com>
2022-12-07 04:39:07 +0000

    Refactor code -- include require -- create test file(s) to seperate testing from function

--- tail.js
+++ tail.js
@@ -1,8 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let pass = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  let fail = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-  return (actual === expected ? console.log(pass) : console.log(fail));
-};
 const tail = function(array) {
   let newArray = []; //empty array for storing 'tail' end of original array.
   if (array === [] || array.length === 0) { //if statement to declare what happens if empty array || single element array
@@ -13,6 +8,5 @@ const tail = function(array) {
   }
   return console.log(newArray);
 };
-const array = ["Yo Yo", "Lighthouse", "Labs"];
-tail(array); // no need to capture the return value since we are not checking it
-assertEqual(array.length, 3); // original array should still have 3 elements!
+
+module.exports = tail;
