6513c4160fc7c23d81669d3b28eb18f1f07d1a26
Daniel Seniv <danielseniv@outlook.com>
2022-12-08 22:34:06 +0000

    Refactor testing function. Use mocha testing library, and chai assertion library

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,14 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-const array = ["Yo Yo", "Lighthouse", "Labs"];
-tail(array); // no need to capture the return value since we are not checking it
-assertEqual(array.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("should return ['Lighthouse', 'Labs']", () => {
+    const array = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(array), ["Lighthouse","Labs"]);
+  })
+  it("should return 2 for the length property", () => {
+    const array = ["Yo Yo", "Lighthouse", "Labs"];
+    const tailArray = tail(array);
+    assert.deepEqual(tailArray.length, 2);
+  })
+})
