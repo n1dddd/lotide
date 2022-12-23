b4a3c0c70ade70bc00a71c858d2cf4f940ee7da8
Daniel Seniv <danielseniv@outlook.com>
2022-12-23 21:14:35 +0000

    Update and remove over-explanatory comments

--- countOnly.js
+++ countOnly.js
@@ -1,11 +1,5 @@
-//Function that takes in two arguments (allItems, itemsToCount)
-//initialize empty object...results
-//Loop over allItems (names in this case)
-//If the boolean value of itemToCount is truthy, increase the counter of said item, else ignore keeping counting
-//If the item has another instance, increase count
-//Else, return 1, or single counted instance
-//Return counts of specific items
-//Return undefined if the itemsToCount does not match any item in the object
+//Function that takes in two arguments (allItems, itemsToCount), checks on two conditionals, return object containing results.
+
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
@@ -23,24 +17,6 @@ const countOnly = function(allItems, itemsToCount) {
 
 module.exports = countOnly;
 
-//DRIVE CODE (ORIGINALLY USED FOR TESTING);
-
-//const firstNames = [
-//   "Karl",
-//   "Salima",
-//   "Agouhanna",
-//   "Fang",
-//   "Kavith",
-//   "Jason",
-//   "Salima",
-//   "Fang",
-//   "Joe"
-// ];
-// const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true, "Agouhanna": false});
-// assertEqual(result1["Jason"], 1);
-// assertEqual(result1["Karima"], undefined);
-// assertEqual(result1["Fang"], 2);
-// assertEqual(result1["Agouhanna"], undefined);
 
 
 
