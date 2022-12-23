c78962dcea322d6f0f6db964c92ce78b0c131c4c
Daniel Seniv <danielseniv@outlook.com>
2022-12-23 22:07:49 +0000

    Update and remove over-explanatory comments

--- without.js
+++ without.js
@@ -1,16 +1,9 @@
-//Function that takes in a source array and itemsToRemove as arguments
-//Initialize a new results array
-//Loop through the source
-//Reference itemsToRemove, and if they are included within the source array, remove them
-//Else, if the reference does not include itemsToRemove, push the source element to the results array
-//Return the results array
+//Function that takes in a source array and itemsToRemove as arguments. Return an array with elements that do not match to itemsToRemove
 
 const without = function(source,itemsToRemove) {
   let newArray = [];
   for (let i = 0; i < source.length; i++) {
-    if (itemsToRemove.includes(source[i])) {
-      //empty as we want to remove source[i]
-    } else {
+    if (!itemsToRemove.includes(source[i])) {
       newArray.push(source[i]);
     }
   }
@@ -18,13 +11,3 @@ const without = function(source,itemsToRemove) {
 };
 
 module.exports = without;
-
-//DRIVE CODE (ORIGINALLY USED FOR TESTING);
-
-// without([1, 2, 3], [1]); // => [2, 3]
-// const words = ["hello", "world", "lighthouse"];
-// without(words, ["lighthouse"]); // no need to capture return value for this test case
-// // Make sure the original array was not altered by the without function
-// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-// assertArraysEqual(words, ["hello", "world"]);
-// assertArraysEqual(without(words,"lighthouse"), ["hello", "world"]);
