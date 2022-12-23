027ccae54516c2ab1bcc105674c6acd1d86da71c
Daniel Seniv <danielseniv@outlook.com>
2022-12-23 22:07:02 +0000

    Update and remove over-explanatory comments

--- takeUntil.js
+++ takeUntil.js
@@ -1,9 +1,4 @@
-//Function that takes in an array, and callback
-//Initialize an empty results array
-//Loop through the elements of the array
-//If the callback returns a boolean false value, push the element into the results array
-//If the callback returns a boolean true value, stop the loop
-//Return the results of the array
+//Function that takes in an array, and callback. Return a transformed array, based on callback.
 
 const takeUntil = function(array, callback) {
   const results = [];
@@ -19,12 +14,4 @@ const takeUntil = function(array, callback) {
 
 module.exports = takeUntil;
 
-//DRIVE CODE (ORIGINALLY USED FOR TESTING);
-
-// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-// const results1 = takeUntil(data1, x => x < 0);
-// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-// const results2 = takeUntil(data2, x => x === ',');
-// assertArraysEqual(results1,[1,2,5,7,2]);
-// assertArraysEqual(results2,['I\'ve', 'been', 'to', 'Hollywood']);
 
