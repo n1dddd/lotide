24e228ee6223a34ce36f7660a440f44c126213c0
Daniel Seniv <danielseniv@outlook.com>
2022-12-23 21:44:58 +0000

    Update and remove over-explanatory comments

--- head.js
+++ head.js
@@ -1,6 +1,4 @@
-//Function that takes in an array as its argument
-//If the array is undefined, return undefined
-//Otherwise, return only the first element of the array (index [0])
+//Function that takes in an array as its argument and return only the first element of the array (index [0])
 
 function head(array) {
   if (array === undefined) {
@@ -9,11 +7,5 @@ function head(array) {
   let firstElement = array[0];
   return firstElement;
 }
-
-//DRIVE CODE (ORIGINALLY USED FOR TESTING);
-
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([]),5);
 
 module.exports = head;
