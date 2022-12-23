74afd8dcb6eacb9bd69c8f26df38e2292b85a33b
Daniel Seniv <danielseniv@outlook.com>
2022-12-23 21:13:11 +0000

    Update and remove over-explanatory comments

--- countLetters.js
+++ countLetters.js
@@ -1,10 +1,4 @@
-//Function countLetters takes in a string (sentence to count)
-//Loop begins over the string
-//If the string is empty, return empty object
-//If NOT empty, [letter] of sentenceToCount becomes property of results object
-//If the letter is encountered in the loop again, the counter property of [letter] increases
-//If letter is only encountered once, return 1.
-//Lastly return the counter of the [letter] property in the results object
+//Function countLetters takes in a string and returns object containing count of letter instance in string
 
 const countLetters = function(sentenceToCount) {
   const results = {};
@@ -23,19 +17,6 @@ const countLetters = function(sentenceToCount) {
 
 module.exports = countLetters;
 
-//DRIVE CODE (ORIGINALLY USED FOR TESTING);
-
-// const results1 = countLetters("lighthouse in the house");
-// assertEqual(results1["l"],1);
-// assertEqual(results1["i"],2);
-// assertEqual(results1["g"],1);
-// assertEqual(results1["h"],4);
-// assertEqual(results1["t"],2);
-// assertEqual(results1["o"],2);
-// assertEqual(results1["u"],2);
-// assertEqual(results1["s"],2);
-// assertEqual(results1["e"],3);
-// assertEqual(results1["n"],1);
 
 
 
