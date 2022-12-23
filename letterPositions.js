77873519177ca7265ed84c0b2b64794319094e18
Daniel Seniv <danielseniv@outlook.com>
2022-12-23 21:48:53 +0000

    Remove unecessary index.js reference. Update and remove over-explanatory comments

--- letterPositions.js
+++ letterPositions.js
@@ -1,13 +1,13 @@
-const _ = require('./index')
+//Function letterPositions takes in string. Returns index position(s) of character within string.
 
-const letterPositions = function(sentence) { //takes in parameter string
-  const results = {}; //empty object to store key pairs
-  for (let i = 0; i < sentence.length; i++) { //loops through string
-    if (sentence[i] !== ' ') { //ignores spaces
-      if (results[sentence[i]]) { //if the index value is the seen again throughout the loop
-        results[sentence[i]].push(i); //pushes the index(s) into the object key, and stored as an array
+const letterPositions = function(sentence) { 
+  const results = {}; 
+  for (let i = 0; i < sentence.length; i++) { 
+    if (sentence[i] !== ' ') { 
+      if (results[sentence[i]]) { 
+        results[sentence[i]].push(i); 
       } else {
-        results[sentence[i]] = [i]; //if only 1 was seen index, provides index value to array
+        results[sentence[i]] = [i]; 
       }
     }
   }
@@ -15,7 +15,3 @@ const letterPositions = function(sentence) { //takes in parameter string
 };
 
 module.exports = letterPositions;
-
-//DRIVE CODE (ORIGINALLY USED FOR TESTING);
-
-//assertArraysEqual(letterPositions("lighthouse in the house").h, [3,5,15,18]);
