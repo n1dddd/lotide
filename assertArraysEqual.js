0cf141bfd6f3c3182aaea5aa3f842b7ca34b2c2d
Daniel Seniv <danielseniv@outlook.com>
2023-01-11 03:41:37 +0000

    Remove assignment of variable to file requirement

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,7 @@
-const _ = require('./eqArrays') //imports required function from index file
+const eqArrays = require('./eqArrays') //imports required function from index file
 
 const assertArraysEqual = function(array1, array2) {
-  if (_.eqArrays(array1,array2)) {  //eqArrays used to determine if arrays are equal
+  if (eqArrays(array1,array2)) {  //eqArrays used to determine if arrays are equal
     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`); //log if assertion passes or fails
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
